ff82501745e263c1e0370754b5250fa1555416ee
Fabian Buentello <fabian.buentello@gmail.com>
2015-10-15 02:29:48 -0500

    feat(attendees): Prevent duplicates in Database

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'production',apiEndpoint:'https://backend.uhcode.red/'})
+.constant('ENV', {name:'development',apiEndpoint:'http://localhost:3000/'})
 
 ;
